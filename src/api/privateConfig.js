d0ac8f8e3da53dbe8a200783d0c1c8b5425cae1b
talalshahh <talalshah27@gmail.com>
2023-01-19 16:15:19 +0500

    bug fixes

--- src/api/privateConfig.js
+++ src/api/privateConfig.js
@@ -1,18 +1,17 @@
-import axios from "axios";
-import { getFBToken } from "../firebase/FBToken";
+import { getFBToken } from '../firebase/FBToken';
+const axios = require('axios').default;
 
 const api = axios.create({
   baseURL: `${process.env.URL}/api/private`,
   timeout: 30000,
   headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
   },
 });
 
 api.interceptors.request.use(
   async function (config) {
-    console.log(process.env.URL, "host");
     const appToken = await getFBToken();
     config.headers.AuthToken = appToken;
     return config;
